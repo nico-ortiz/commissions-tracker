0e109a44ea0ad5f1870b13364c65a56cceded99f
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-11-16 17:03:38 -0300

    Initial commit

---
+++ src/test/java/com/goldeng/commissionstracker/CommissionsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldeng.commissionstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommissionsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
