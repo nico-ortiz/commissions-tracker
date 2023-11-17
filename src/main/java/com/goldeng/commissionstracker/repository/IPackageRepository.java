d45fc6b171ef368e98c29a4d9659cabd664cb395
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-11-17 17:52:55 -0300

    Modify Customer's and Package's repository. Ref #3

--- src/main/java/com/goldeng/commissionstracker/repository/IPackageRepository.java
+++ src/main/java/com/goldeng/commissionstracker/repository/IPackageRepository.java
@@ -3,6 +3,8 @@ package com.goldeng.commissionstracker.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.goldeng.commissionstracker.model.Package;
+
 @Repository
 public interface IPackageRepository extends JpaRepository<Package, Long>{
     
