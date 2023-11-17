25ccd506b299788cc50fd68c4bc468c0fbbcf0b4
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-11-17 14:52:38 -0300

    Create Customer model. Ref #1

---
+++ src/main/java/com/goldeng/commissionstracker/model/Customer.java
@@ -0,0 +1,53 @@
+package com.goldeng.commissionstracker.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Customer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+    
+    @Column(unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    private String address;
+    
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Commission> commissions;
+
+    public boolean addCommission(Commission c) {
+        return this.commissions.add(c);
+    }
+
+    public boolean removeCommission(Commission c) {
+        return this.commissions.remove(c);
+    }
+}
